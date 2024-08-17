885bcb48e85515446fadfcdb181e24ef122be71d
Irving Amaechi <leoivin0@gmail.com>
2024-08-17 14:51:20 +0100

    Top selling by quantity analysis

--- analysis/views.py
+++ analysis/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import SalesDataForm
-from .utils.analysis import process_sales_file, sales_by_month_analysis, top_selling_products_analysis
+from .utils.analysis import (process_sales_file, sales_by_month_analysis, 
+                             top_selling_products_analysis, top_selling_by_total_sales_analysis)
 
 def upload_sales_data(request):
     if request.method == 'POST':
@@ -18,8 +19,11 @@ def upload_sales_data(request):
                 # Perform top selling products by quantity analysis
                 top_selling_products, summary_products, fig_products = top_selling_products_analysis(sales_data.file.path)
 
+                # Perform top selling products by total sales analysis
+                top_selling_sales, summary_sales, fig_sales = top_selling_by_total_sales_analysis(sales_data.file.path)
+
                 # Check if the summary is populated correctly
-                if not (summary and summary_month and summary_products):
+                if not (summary and summary_month and summary_products and summary_sales):
                     raise ValueError("Summary data is missing or invalid.")
                 
                 context = {
@@ -27,7 +31,9 @@ def upload_sales_data(request):
                     'summary_month': summary_month,
                     "fig_month": fig_month.to_json(),
                     "summary_products": summary_products,
-                    "fig_products": fig_products.to_json()
+                    "fig_products": fig_products.to_json(),
+                    "summary_sales": summary_sales,
+                    "fig_sales": fig_sales.to_json()
                 }
                 
                 return render(request, 'upload_success.html', context)
