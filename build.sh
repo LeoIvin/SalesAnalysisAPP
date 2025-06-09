404a9e2869b9744a41a07ccde87143d4a4108ac0
Irving Amaechi <leoivin0@gmail.com>
2025-06-10 00:40:21 +0100

    Latest render deployment

--- build.sh
+++ build.sh
@@ -2,15 +2,8 @@
 # exit on error
 set -o errexit
 
-<<<<<<< HEAD
-pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-python manage.py migrate 
-=======
 pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
->>>>>>> 6eea30f94a8441445992e569ec24f7115bfefe95
